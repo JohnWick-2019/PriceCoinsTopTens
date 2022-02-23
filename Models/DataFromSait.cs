b6755bdcd1f97361b3a31c8a5a0f739a24b79f63
JohnWick <oleg26vrn@gmail.com>
2022-02-23 12:09:38 +0300

    Add model DataFromSait. Create four properties.

---
+++ Models/DataFromSait.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCoinsTopTens.Models
+{
+    public class DataFromSait
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string CoinName { get; set; }
+        public string CoinPrice { get; set; }
+    }
+}
